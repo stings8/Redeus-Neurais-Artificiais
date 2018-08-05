c1afe6029c8b12d1da2418a2e24a493fdf6a8631
Sting Silva <stingufc@gmail.com>
2018-08-05 01:35:14 -0300

    treino iris

--- multclasse/iris_simples.py
+++ multclasse/iris_simples.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  5 00:51:41 2018
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
 
-@author: sting
-"""
+base = pd.read_csv('iris.csv')
 
+previsores = base.iloc[:, 0:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+previsores_treino, previsores_teste, classe_treino, classe_teste = train_test_split(previsores, classe, test_size= 0.25)
+
+classificador = Sequential()
+classificador.add(Dense(units= 4, activation= 'relu', input_dim = 4))
+classificador.add(Dense(units= 4, activation= 'relu'))
+classificador.add(Dense(units= 3, activation= 'softmax'))
+classificador.compile(optimizer= 'adam', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'])
+
+classificador.fit(previsores_treino, classe_treino, batch_size= 10, epochs = 1000)
