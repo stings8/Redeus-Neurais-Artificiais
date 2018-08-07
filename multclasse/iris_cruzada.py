abb4f01e2746643622a5b675ed8d43190235fd0e
Sting Silva <stingufc@gmail.com>
2018-08-06 21:23:37 -0300

    validacao cruzada

--- multclasse/iris_cruzada.py
+++ multclasse/iris_cruzada.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 base = pd.read_csv('iris.csv')
 previsores = base.iloc[:, 0:4].values
@@ -14,17 +15,23 @@ labelencoder = LabelEncoder()
 classe = labelencoder.fit_transform(classe)
 classe_dummy = np_utils.to_categorical(classe)
 
-def criar_rede():
+def criar_rede(activation, dropout, neurons):
     classificador = Sequential()
-    classificador.add(Dense(units= 4, activation= 'relu', input_dim = 4))
-    classificador.add(Dense(units= 4, activation= 'relu'))
+    classificador.add(Dense(units= neurons, activation= activation, input_dim = 4))
+    classificador.add(Dropout(dropout))
     classificador.add(Dense(units= 3, activation= 'softmax'))
-    classificador.compile(optimizer= 'adam', loss = 'categorical_crossentropy', metrics = ['categorical_accuracy'])
+    classificador.compile(optimizer= 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
     return classificador
 
-classificador = KerasClassifier(build_fn= criar_rede, epochs = 1000,
-                                batch_size = 10)
-resultados = cross_val_score(estimator= classificador, X= previsores, y= classe,
-                             cv = 10, scoring= 'accuracy')
-media = resultados.mean()
-desvio = resultados.std()
+classificador = KerasClassifier(build_fn= criar_rede)
+
+parametros = {'batch_size': [10, 12, 15],
+              'neurons': [3, 4, 5],
+              'dropout': [0.1, 0.2, 0.3],
+              'epochs': [500, 1000, 1500],
+              'activation': ['relu', 'sigmoid', 'softmax']}
+
+grid_search = GridSearchCV(estimator= classificador, param_grid= parametros, cv = 5)
+grid_search = grid_search.fit(previsores, classe)
+melhores_param = grid_search.best_params_
+melhor_preci = grid_search.best_score_
